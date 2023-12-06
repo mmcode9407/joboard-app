3158da5074010d164f4777f12793d59ff9d05ae0
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2023-12-06 15:07:30 +0100

    feat: add filters clearing

--- src/components/Filter/FilterBox.tsx
+++ src/components/Filter/FilterBox.tsx
@@ -3,13 +3,18 @@ import CheckboxFilterItem from './CheckboxFilterItem';
 import { filterSections } from './filters/filters';
 import { FilterGroup } from '../../types/types';
 import RangeFilterItem from './RangeFilterItem';
+import { useFilters } from '../../context/FiltersContext';
 
 export const FilterBox = () => {
+   const { clearFilters } = useFilters();
+
    return (
       <div className="w-[303px] p-6 rounded-md border border-gray-light">
          <header className="flex items-center justify-between mb-4">
             <h2 className="text-gray-darkest text-sb-16">Filter offers</h2>
-            <button className=" w-[95] p-2  text-accent-strong text-md-12">Clear filters</button>
+            <button className=" w-[95] p-2  text-accent-strong text-md-12" onClick={clearFilters}>
+               Clear filters
+            </button>
          </header>
          <CheckboxFilterItem
             title="Job type"
